1f7e99dda082f0145c77f8c3abe1429c2d9a6f09
elizabeth <toelizabethes@gmail.com>
2021-12-04 12:56:40 +0300

    Added CPU, GPU, Memory info :racehorse:

--- fetch.sh
+++ fetch.sh
@@ -30,7 +30,10 @@ get_packages () {
 
 get_shell () {
 	shell_="$(basename $SHELL)"
-	shell_ver="${BASH_VERSION}"
+	if [ "$shell_" = "bash" ]
+	then
+		shell_="bash $(bash --version | head -1 | cut -d ' ' -f 4 | cut -d '(' -f 1)"
+	fi
 }
 
 get_resolution () {
@@ -42,6 +45,20 @@ get_de () {
 	de_ver="$(plasmashell --version | awk '{print $2}')"
 }
 
+get_gpu () {
+	cpu_="$(cat /proc/cpuinfo | grep 'model name' | head -1 | cut -d':' -f 2 | xargs)"
+}
+
+get_cpu () {
+	gpu_="$(lspci | grep VGA | cut -d '[' -f 3)"
+	gpu_="$(lspci -vnn | grep VGA -A 12 | grep Subsystem | cut -f 2 | cut -d ']' -f 1 | cut -d ' ' -f2-)]"
+}
+
+get_memory () {
+	used_memory="$(grep -i MemAvailable /proc/meminfo | awk '{print $2}')"
+	total_memory=$(grep -i MemTotal /proc/meminfo | awk '{print $2}')
+	total_memory="$((used_memory/1024)) / $((total_memory/1024)) MiB"
+}
 
 
 get_name
@@ -52,7 +69,9 @@ get_packages
 get_shell
 get_resolution
 get_de
-
+get_gpu
+get_cpu
+get_memory
 
 
 main () {
@@ -60,17 +79,17 @@ main () {
 	then
 printf \
 "$red           ..          			$blue $name
-$red       ##############    		$green ##############
+$red       ##############    		$green ################
 $red    ####           (###* 		$blue Operating System: $white$distro_
 $red  *##                ##/(		$blue Kernel: $white$kernel_version
 $red ##*       *(    .    ## 		$blue Uptime: $white${uptime_:3}
 $red ##       #           ## 		$blue Packages: $white $pk_num
-$red #        #       .   #. 		$blue Shell: $white$shell_ $shell_ver
+$red #        #       .   #. 		$blue Shell: $white$shell_
 $red ##       .#,       ##   		$blue Resolution: $white$screen_res
 $red (#          (,/(*       		$blue Desktop Environment: $white$de_ $de_ver
-$red  ###                    		
-$red    ##                   
-$red      ##                 
+$red  ###                    		$blue CPU: $white$cpu_
+$red    ##                   		$blue GPU: $white$gpu_
+$red      ##                 		$blue Memory: $white$total_memory
 $red         ,#*             
 $white
 		\n"
